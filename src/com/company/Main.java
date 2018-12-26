9bb08bddb7e9cd177b99aa427758379173685623
ChadoDiVanci <46169479+ChadoDiVanci@users.noreply.github.com>
2018-12-26 21:46:40 +0100

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Client {
+        String client;
+        int account;
+        boolean info_credit;
+        String pin;
+
+        Client()
+        {
+
+        }
+    public static void  main(String args[])
+    {
+        Client client_1 = new Client();
+
+    }
+
+
+}
